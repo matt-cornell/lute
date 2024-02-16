19064d018ccb7618a16f90ca3fb3b20dd0a6d967
Matthew Cornell <mcornell400708@gmail.com>
2024-02-16 11:51:52 -0500

    Improve SMILES parser CLI

--- src/bin/parse-smiles.rs
+++ src/bin/parse-smiles.rs
@@ -1,4 +1,7 @@
 use clap::{Parser, ValueEnum};
+use std::fmt::{self, Display, Formatter};
+use std::path::{Path, PathBuf};
+use std::io::{Write, stdout};
 
 #[derive(Debug, Default, Clone, Copy, ValueEnum)]
 enum OutputType {
@@ -7,22 +10,44 @@ enum OutputType {
     Svg,
     Png,
 }
+impl Display for OutputType {
+    fn fmt(&self, f: &mut Formatter<'_>) -> fmt::Result {
+        match self {
+            Self::Dot => f.write_str("dot"),
+            Self::Svg => f.write_str("svg"),
+            Self::Png => f.write_str("png"),
+        }
+    }
+}
 
 #[derive(Parser)]
 #[command(version, about)]
 struct Cli {
+    #[arg(short, long, default_value_t = OutputType::Dot)]
+    fmt: OutputType,
     #[arg(short, long)]
-    out: OutputType,
+    out: Option<PathBuf>,
     input: String,
+}
+
+fn write_output<O: Display>(path: Option<&Path>, out: O) {
+    let res = if let Some(p) = path {
+        std::fs::File::create(p).and_then(|mut f| write!(f, "{}", out))
+    } else {
+        write!(stdout(), "{}", out)
+    };
+    if let Err(err) = res {
+        eprintln!("{err}");
+    }
 }
 
 fn main() {
     let cli = Cli::parse();
     let parser = chem_sim::parse::SmilesParser::new_unsuppressed(&cli.input);
     match parser.parse() {
-        Ok(graph) => match cli.out {
-            OutputType::Dot => println!("{}", chem_sim::disp::fmt_as_dot(&graph)),
-            OutputType::Svg => println!("{}", chem_sim::disp::fmt_as_svg(&graph)),
+        Ok(graph) => match cli.fmt {
+            OutputType::Dot => write_output(cli.out.as_deref(), chem_sim::disp::fmt_as_dot(&graph)),
+            OutputType::Svg => write_output(cli.out.as_deref(), chem_sim::disp::fmt_as_svg(&graph)),
             OutputType::Png => todo!(),
         },
         Err(err) => eprintln!("{err}"),
