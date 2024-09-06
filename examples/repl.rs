54d1300d18ce959478198847e3f276475278cc42
Matthew Cornell <mcornell400708@gmail.com>
2024-09-06 21:52:26 +0000

    Add a graph union, more output types, and settings

--- examples/repl.rs
+++ examples/repl.rs
@@ -1,10 +1,14 @@
+use clap::builder::PossibleValue;
+use lute::graph::*;
 use lute::prelude::*;
+use petgraph::prelude::*;
 use reedline_repl_rs::{self as rlr, Repl};
 use rlr::clap::{self, Arg, ArgAction, ArgMatches, Command};
-use clap::builder::PossibleValue;
-use std::convert::Infallible;
-use std::path::PathBuf;
 use std::env;
+use std::ffi::OsStr;
+use std::path::PathBuf;
+use std::sync::Arc;
+use std::time::Instant;
 use thiserror::Error;
 use tracing_subscriber::filter::{LevelFilter, ParseError, Targets};
 use tracing_subscriber::fmt::{self, format::*};
@@ -26,7 +30,7 @@ impl clap::ValueEnum for DumpType {
         {
             #[cfg(feature = "resvg")]
             return &[Self::FastSmiles, Self::CanonSmiles, Self::Svg, Self::Png];
-            return &[Self::FastSmiles, Self::CanonSmiles, Self::Svg]
+            return &[Self::FastSmiles, Self::CanonSmiles, Self::Svg];
         }
         &[Self::FastSmiles, Self::CanonSmiles]
     }
@@ -40,7 +44,6 @@ impl clap::ValueEnum for DumpType {
             Self::Png => Some(PossibleValue::new("png")),
         }
     }
-
 }
 
 #[derive(Debug, Error)]
@@ -56,6 +59,100 @@ enum ReplError {
         index: usize,
         err: lute::parse::smiles::SmilesError,
     },
+    #[error("Attempted to generate a PNG of a molecule without an output path")]
+    PngToStdout,
+    /// other stuff I don't wanna name
+    #[error(transparent)]
+    Other(#[from] Box<dyn std::error::Error + Send + Sync>),
+}
+
+#[derive(Debug, Clone, Copy, PartialEq)]
+enum FlagKind {
+    Enable,
+    Disable,
+    Query,
+}
+
+#[derive(Debug, Clone, Copy, PartialEq)]
+enum FlagName {
+    Timings,
+    LegacyRender,
+}
+
+#[derive(Debug, Clone, Copy)]
+struct SetParser;
+impl clap::builder::TypedValueParser for SetParser {
+    type Value = (FlagKind, FlagName);
+
+    fn parse_ref(
+        &self,
+        cmd: &Command,
+        _arg: Option<&Arg>,
+        value: &OsStr,
+    ) -> Result<Self::Value, clap::Error> {
+        use clap::error::*;
+        let val = value
+            .to_str()
+            .ok_or_else(|| Error::new(ErrorKind::InvalidUtf8).with_cmd(cmd))?;
+        let mode = match val.as_bytes().get(0) {
+            Some(&b'+') => FlagKind::Enable,
+            Some(&b'-') => FlagKind::Disable,
+            Some(&b'?') => FlagKind::Query,
+            _ => {
+                let mut err = Error::new(ErrorKind::ValueValidation).with_cmd(cmd);
+                err.insert(ContextKind::InvalidValue, ContextValue::String(val.to_string()));
+                err.insert(
+                    ContextKind::Usage,
+                    ContextValue::StyledStr(
+                        "Make sure the argument starts with '+', '-', or '?'".into(),
+                    ),
+                );
+                return Err(err);
+            }
+        };
+        let name = match &val[1..] {
+            "timings" => FlagName::Timings,
+            "legacy-render" | "legacy_render" => FlagName::LegacyRender,
+            _ => {
+                let mut err = Error::new(ErrorKind::ValueValidation).with_cmd(cmd);
+                err.insert(ContextKind::InvalidValue, ContextValue::String(val.to_string()));
+                err.insert(
+                    ContextKind::Usage,
+                    ContextValue::StyledStr(
+                        r#"Available flags are "timings" and "legacy-render""#.into(),
+                    ),
+                );
+                return Err(err);
+            }
+        };
+        Ok((mode, name))
+    }
+    fn possible_values(&self) -> Option<Box<dyn Iterator<Item = PossibleValue>>> {
+        Some(
+            Box::new([
+                PossibleValue::new("+timings").hide(true),
+                PossibleValue::new("-timings").hide(true),
+                PossibleValue::new("?timings"),
+                PossibleValue::new("+legacy-render").alias("+legacy_render").hide(true),
+                PossibleValue::new("-legacy-render").alias("-legacy_render").hide(true),
+                PossibleValue::new("?legacy-render").alias("?legacy_render"),
+            ]
+            .into_iter()),
+        )
+    }
+}
+
+#[derive(Debug, Clone)]
+struct Timer(Instant);
+impl Timer {
+    pub fn new() -> Self {
+        Self(Instant::now())
+    }
+}
+impl Drop for Timer {
+    fn drop(&mut self) {
+        println!("{:?}", self.0.elapsed());
+    }
 }
 
 type SubscriberType = tracing_subscriber::layer::Layered<
@@ -65,7 +162,9 @@ type SubscriberType = tracing_subscriber::layer::Layered<
 
 struct Context {
     arena: Arena<u32>,
-    trace: SubscriberType,
+    trace: Arc<SubscriberType>,
+    timings: bool,
+    legacy_render: bool,
 }
 
 fn default_subscriber() -> SubscriberType {
@@ -84,12 +183,20 @@ fn make_subscriber(filter: &str) -> Result<SubscriberType, ParseError> {
 }
 
 fn set_log(args: ArgMatches, ctx: &mut Context) -> Result<Option<String>, ReplError> {
-    ctx.trace = make_subscriber(args.get_one::<String>("filter").unwrap())?;
-    Ok(None)
+    if let Some(filter) = args.get_one::<String>("filter") {
+        ctx.trace = Arc::new(make_subscriber(filter)?);
+        Ok(None)
+    } else {
+        Ok(Some(
+            ctx.trace.downcast_ref::<Targets>().unwrap().to_string(),
+        ))
+    }
 }
 
 fn load_smiles(args: ArgMatches, ctx: &mut Context) -> Result<Option<String>, ReplError> {
     use std::fmt::Write;
+    let _guard = tracing::subscriber::set_default(ctx.trace.clone());
+    let _timer = ctx.timings.then(Timer::new);
     let mut out = String::new();
     let inputs = args.get_many::<String>("input").unwrap();
     let len = inputs.len();
@@ -98,7 +205,7 @@ fn load_smiles(args: ArgMatches, ctx: &mut Context) -> Result<Option<String>, Re
             .parse()
             .map_err(|err| ReplError::Smiles {
                 err,
-                index: if len == 1 { 0 } else { len },
+                index: if len == 1 { 0 } else { n },
             })?;
         let id = ctx.arena.insert_mol(&graph);
         if !out.is_empty() {
@@ -110,6 +217,8 @@ fn load_smiles(args: ArgMatches, ctx: &mut Context) -> Result<Option<String>, Re
 }
 
 fn dump(args: ArgMatches, ctx: &mut Context) -> Result<Option<String>, ReplError> {
+    let _guard = tracing::subscriber::set_default(ctx.trace.clone());
+    let _timer = ctx.timings.then(Timer::new);
     let path = args.get_one::<PathBuf>("out");
     match args.get_one::<DumpType>("type").unwrap() {
         ty @ (DumpType::CanonSmiles | DumpType::FastSmiles) => {
@@ -119,7 +228,10 @@ fn dump(args: ArgMatches, ctx: &mut Context) -> Result<Option<String>, ReplError
                 SmilesConfig::new()
             };
             let s = if let Some(inputs) = args.get_many::<u32>("input") {
-                todo!()
+                generate_smiles(
+                    &GraphUnion(inputs.map(|i| ctx.arena.molecule(*i)).collect()),
+                    cfg,
+                )
             } else {
                 generate_smiles(ctx.arena.graph(), cfg)
             };
@@ -130,18 +242,93 @@ fn dump(args: ArgMatches, ctx: &mut Context) -> Result<Option<String>, ReplError
                 Ok(Some(s))
             }
         }
-        _ => unreachable!(),
+        #[cfg(feature = "coordgen")]
+        DumpType::Svg => {
+            use lute::disp::svg::FormatMode;
+            let mode = if ctx.legacy_render {
+                FormatMode::LegacyH
+            } else {
+                FormatMode::Normal
+            };
+            let s = if let Some(inputs) = args.get_many::<u32>("input") {
+                SvgFormatter {
+                    graph: &GraphUnion(inputs.map(|i| ctx.arena.molecule(*i)).collect()),
+                    mode,
+                }
+                .to_string()
+            } else {
+                SvgFormatter {
+                    graph: &GraphCompactor::<&StableUnGraph<Atom, Bond>>::new(ctx.arena.graph()),
+                    mode,
+                }
+                .to_string()
+            };
+            if let Some(p) = path {
+                std::fs::write(&p, s)?;
+                Ok(Some(format!("saved to {}", p.display())))
+            } else {
+                Ok(Some(s))
+            }
+        }
+        #[cfg(all(feature = "coordgen", feature = "resvg"))]
+        DumpType::Png => {
+            use lute::disp::svg::FormatMode;
+            let mode = if ctx.legacy_render {
+                FormatMode::LegacyH
+            } else {
+                FormatMode::Normal
+            };
+            let s = if let Some(inputs) = args.get_many::<u32>("input") {
+                SvgFormatter {
+                    graph: &GraphUnion(inputs.map(|i| ctx.arena.molecule(*i)).collect()),
+                    mode,
+                }
+                .render(None)
+            } else {
+                SvgFormatter {
+                    graph: &GraphCompactor::<&StableUnGraph<Atom, Bond>>::new(ctx.arena.graph()),
+                    mode,
+                }
+                .render(None)
+            };
+            if let Some(p) = path {
+                s.save_png(&p).map_err(Box::from)?;
+                Ok(Some(format!("saved to {}", p.display())))
+            } else {
+                Err(ReplError::PngToStdout)
+            }
+        }
     }
+}
+
+fn adjust_settings(args: ArgMatches, ctx: &mut Context) -> Result<Option<String>, ReplError> {
+    use std::fmt::Write;
+    let mut out = String::new();
+    for (act, name) in args.get_many::<(FlagKind, FlagName)>("settings").unwrap() {
+        let flag = match name {
+            FlagName::Timings => &mut ctx.timings,
+            FlagName::LegacyRender => &mut ctx.legacy_render,
+        };
+        match act {
+            FlagKind::Enable => *flag = true,
+            FlagKind::Disable => *flag = false,
+            FlagKind::Query => {
+                if out.is_empty() {
+                    out.push(' ');
+                    let _ = write!(out, "{flag}");
+                }
+            }
+        }
+    }
+    Ok(Some(out))
 }
 
 fn main() {
     let trace = match env::var("LUTE_LOG") {
-        Ok(name) => {
-            make_subscriber(&name).unwrap_or_else(|e| {
-                eprintln!("Error in LUTE_LOG environment variable: {e}");
-                default_subscriber()
-            })
-        }
+        Ok(name) => make_subscriber(&name).unwrap_or_else(|e| {
+            eprintln!("Error in LUTE_LOG environment variable: {e}");
+            default_subscriber()
+        }),
         Err(env::VarError::NotPresent) => default_subscriber(),
         Err(env::VarError::NotUnicode(_)) => {
             eprintln!("Error in LUTE_LOG environment variable: not valid UTF-8");
@@ -150,10 +337,14 @@ fn main() {
     };
     let context = Context {
         arena: Arena::new(),
-        trace,
+        trace: Arc::new(trace),
+        timings: false,
+        legacy_render: false,
     };
     let mut repl = Repl::new(context)
-        .with_name("Lute")
+        .with_name("lute")
+        .with_version(concat!("v", env!("CARGO_PKG_VERSION")))
+        .with_prompt(concat!("lute v", env!("CARGO_PKG_VERSION")))
         .with_command(Command::new("reset").about("Reset the arena"), |_, ctx| {
             ctx.arena = Arena::new();
             Ok(None)
@@ -161,27 +352,54 @@ fn main() {
         .with_command(
             Command::new("log")
                 .about("Set the log filter")
-                .arg(Arg::new("filter").required(true)),
+                .arg(Arg::new("filter").required(false).help("The filter string to use")),
             set_log,
         )
         .with_command(
             Command::new("load")
                 .about("Load SMILES inputs")
-                .arg(Arg::new("input").required(true).action(ArgAction::Append)),
+                .arg(Arg::new("input").required(true).action(ArgAction::Append).help("SMILES inputs to load")),
             load_smiles,
         )
         .with_command(
             Command::new("dump")
                 .about("Show the output of inputs, or the whole graph if no ids are given")
-                .arg(Arg::new("type").required(true).value_parser(clap::value_parser!(DumpType)))
-                .arg(Arg::new("out").short('o').long("output").value_parser(clap::value_parser!(PathBuf)))
+                .arg(
+                    Arg::new("type")
+                        .required(true)
+                        .value_parser(clap::value_parser!(DumpType))
+                        .help("Output format to dump as"),
+                )
+                .arg(
+                    Arg::new("out")
+                        .short('o')
+                        .long("output")
+                        .value_name("FILE")
+                        .value_parser(clap::value_parser!(PathBuf))
+                        .help("The file to output to"),
+                )
                 .arg(
                     Arg::new("input")
                         .required(false)
                         .action(ArgAction::Append)
-                        .value_parser(clap::value_parser!(u32)),
+                        .value_name("IDS")
+                        .value_parser(clap::value_parser!(u32))
+                        .help("Fragment IDs to dump"),
                 ),
             dump,
+        )
+        .with_command(
+            Command::new("set").about("Adjust REPL settings").arg(
+                Arg::new("settings")
+                    .required(true)
+                    .action(ArgAction::Append)
+                    .allow_hyphen_values(true)
+                    .value_parser(clap::builder::ValueParser::new(SetParser)),
+            ),
+            adjust_settings,
         );
-    repl.run();
+    let res = repl.run();
+    if let Err(err) = res {
+        eprintln!("{err}");
+    }
 }
---
+++ src/graph/graph_union.rs
@@ -0,0 +1,318 @@
+use crate::prelude::DataValueMap;
+use petgraph::data::*;
+use petgraph::visit::*;
+use petgraph::Direction;
+
+/// A union of multiple graphs that acts as one, disconnected graph.
+#[derive(Debug, Default, Clone)]
+pub struct GraphUnion<G>(pub Vec<G>);
+
+#[derive(Debug, Clone, Copy, PartialEq, Eq, PartialOrd, Ord, Hash)]
+pub struct Tagged<I> {
+    graph_id: usize,
+    inner: I,
+}
+impl<I: NodeRef> NodeRef for Tagged<I> {
+    type NodeId = Tagged<I::NodeId>;
+    type Weight = I::Weight;
+
+    fn id(&self) -> Self::NodeId {
+        Tagged {
+            graph_id: self.graph_id,
+            inner: self.inner.id(),
+        }
+    }
+    fn weight(&self) -> &Self::Weight {
+        self.inner.weight()
+    }
+}
+impl<I: EdgeRef> EdgeRef for Tagged<I> {
+    type NodeId = Tagged<I::NodeId>;
+    type EdgeId = Tagged<I::EdgeId>;
+    type Weight = I::Weight;
+
+    fn id(&self) -> Self::EdgeId {
+        Tagged {
+            graph_id: self.graph_id,
+            inner: self.inner.id(),
+        }
+    }
+    fn source(&self) -> Self::NodeId {
+        Tagged {
+            graph_id: self.graph_id,
+            inner: self.inner.source(),
+        }
+    }
+    fn target(&self) -> Self::NodeId {
+        Tagged {
+            graph_id: self.graph_id,
+            inner: self.inner.target(),
+        }
+    }
+    fn weight(&self) -> &Self::Weight {
+        self.inner.weight()
+    }
+}
+
+impl<G: GraphBase> GraphBase for GraphUnion<G> {
+    type NodeId = Tagged<G::NodeId>;
+    type EdgeId = Tagged<G::EdgeId>;
+}
+impl<G: GraphProp> GraphProp for GraphUnion<G> {
+    type EdgeType = G::EdgeType;
+}
+impl<G: Data> Data for GraphUnion<G> {
+    type NodeWeight = G::NodeWeight;
+    type EdgeWeight = G::EdgeWeight;
+}
+impl<G: DataMap> DataMap for GraphUnion<G> {
+    fn node_weight(&self, id: Tagged<G::NodeId>) -> Option<&G::NodeWeight> {
+        self.0[id.graph_id].node_weight(id.inner)
+    }
+    fn edge_weight(&self, id: Tagged<G::EdgeId>) -> Option<&G::EdgeWeight> {
+        self.0[id.graph_id].edge_weight(id.inner)
+    }
+}
+impl<G: DataMapMut> DataMapMut for GraphUnion<G> {
+    fn node_weight_mut(&mut self, id: Tagged<G::NodeId>) -> Option<&mut G::NodeWeight> {
+        self.0[id.graph_id].node_weight_mut(id.inner)
+    }
+    fn edge_weight_mut(&mut self, id: Tagged<G::EdgeId>) -> Option<&mut G::EdgeWeight> {
+        self.0[id.graph_id].edge_weight_mut(id.inner)
+    }
+}
+impl<G: DataValueMap> DataValueMap for GraphUnion<G> {
+    fn node_weight(&self, id: Tagged<G::NodeId>) -> Option<G::NodeWeight> {
+        self.0[id.graph_id].node_weight(id.inner)
+    }
+    fn edge_weight(&self, id: Tagged<G::EdgeId>) -> Option<G::EdgeWeight> {
+        self.0[id.graph_id].edge_weight(id.inner)
+    }
+}
+impl<G: NodeCount> NodeCount for GraphUnion<G> {
+    fn node_count(&self) -> usize {
+        self.0.iter().map(G::node_count).sum()
+    }
+}
+impl<G: EdgeCount> EdgeCount for GraphUnion<G> {
+    fn edge_count(&self) -> usize {
+        self.0.iter().map(G::edge_count).sum()
+    }
+}
+impl<G: NodeIndexable> NodeIndexable for GraphUnion<G> {
+    fn to_index(&self, a: Self::NodeId) -> usize {
+        (0..a.graph_id)
+            .map(|i| self.0[i].node_bound())
+            .sum::<usize>()
+            + self.0[a.graph_id].to_index(a.inner)
+    }
+    fn from_index(&self, mut i: usize) -> Self::NodeId {
+        let mut graph_id = 0;
+        for g in &self.0 {
+            if let Some(i2) = i.checked_sub(g.node_bound()) {
+                i = i2;
+                graph_id += 1;
+            } else {
+                break;
+            }
+        }
+        Self::NodeId {
+            graph_id,
+            inner: self.0[graph_id].from_index(i),
+        }
+    }
+    fn node_bound(&self) -> usize {
+        self.0.iter().map(G::node_bound).sum()
+    }
+}
+impl<G: EdgeIndexable> EdgeIndexable for GraphUnion<G> {
+    fn to_index(&self, a: Self::EdgeId) -> usize {
+        (0..a.graph_id)
+            .map(|i| self.0[i].edge_bound())
+            .sum::<usize>()
+            + self.0[a.graph_id].to_index(a.inner)
+    }
+    fn from_index(&self, mut i: usize) -> Self::EdgeId {
+        let mut graph_id = 0;
+        for g in &self.0 {
+            if let Some(i2) = i.checked_sub(g.edge_bound()) {
+                i = i2;
+                graph_id += 1;
+            } else {
+                break;
+            }
+        }
+        Self::EdgeId {
+            graph_id,
+            inner: self.0[graph_id].from_index(i),
+        }
+    }
+    fn edge_bound(&self) -> usize {
+        self.0.iter().map(G::edge_bound).sum()
+    }
+}
+impl<G: NodeCompactIndexable> NodeCompactIndexable for GraphUnion<G> {}
+
+impl<'a, G: GraphBase> IntoNodeIdentifiers for &'a GraphUnion<G>
+where
+    &'a G: IntoNodeIdentifiers<NodeId = G::NodeId>,
+{
+    type NodeIdentifiers = iter::TaggedIter<'a, G, <&'a G as IntoNodeIdentifiers>::NodeIdentifiers>;
+
+    fn node_identifiers(self) -> Self::NodeIdentifiers {
+        iter::TaggedIter {
+            graphs: &self.0,
+            index: 0,
+            iter: self.0.first().map(IntoNodeIdentifiers::node_identifiers),
+            map: IntoNodeIdentifiers::node_identifiers,
+        }
+    }
+}
+impl<'a, G: Data> IntoNodeReferences for &'a GraphUnion<G>
+where
+    &'a G: IntoNodeReferences<NodeId = G::NodeId, NodeWeight = G::NodeWeight>,
+{
+    type NodeRef = Tagged<<&'a G as IntoNodeReferences>::NodeRef>;
+    type NodeReferences = iter::TaggedIter<'a, G, <&'a G as IntoNodeReferences>::NodeReferences>;
+
+    fn node_references(self) -> Self::NodeReferences {
+        iter::TaggedIter {
+            graphs: &self.0,
+            index: 0,
+            iter: self.0.first().map(IntoNodeReferences::node_references),
+            map: IntoNodeReferences::node_references,
+        }
+    }
+}
+impl<'a, G: Data> IntoEdgeReferences for &'a GraphUnion<G>
+where
+    &'a G: IntoEdgeReferences<NodeId = G::NodeId, EdgeId = G::EdgeId, EdgeWeight = G::EdgeWeight>,
+{
+    type EdgeRef = Tagged<<&'a G as IntoEdgeReferences>::EdgeRef>;
+    type EdgeReferences = iter::TaggedIter<'a, G, <&'a G as IntoEdgeReferences>::EdgeReferences>;
+
+    fn edge_references(self) -> Self::EdgeReferences {
+        iter::TaggedIter {
+            graphs: &self.0,
+            index: 0,
+            iter: self.0.first().map(IntoEdgeReferences::edge_references),
+            map: IntoEdgeReferences::edge_references,
+        }
+    }
+}
+impl<'a, G: GraphBase> IntoNeighbors for &'a GraphUnion<G>
+where
+    &'a G: IntoNeighbors<NodeId = G::NodeId>,
+{
+    type Neighbors = iter::MappedIter<<&'a G as IntoNeighbors>::Neighbors>;
+
+    fn neighbors(self, a: Self::NodeId) -> Self::Neighbors {
+        iter::MappedIter {
+            graph_id: a.graph_id,
+            iter: self.0[a.graph_id].neighbors(a.inner),
+        }
+    }
+}
+impl<'a, G: GraphBase> IntoNeighborsDirected for &'a GraphUnion<G>
+where
+    &'a G: IntoNeighborsDirected<NodeId = G::NodeId>,
+{
+    type NeighborsDirected = iter::MappedIter<<&'a G as IntoNeighborsDirected>::NeighborsDirected>;
+
+    fn neighbors_directed(self, a: Self::NodeId, dir: Direction) -> Self::NeighborsDirected {
+        iter::MappedIter {
+            graph_id: a.graph_id,
+            iter: self.0[a.graph_id].neighbors_directed(a.inner, dir),
+        }
+    }
+}
+impl<'a, G: Data> IntoEdges for &'a GraphUnion<G>
+where
+    &'a G: IntoEdges<NodeId = G::NodeId, EdgeId = G::EdgeId, EdgeWeight = G::EdgeWeight>,
+{
+    type Edges = iter::MappedIter<<&'a G as IntoEdges>::Edges>;
+
+    fn edges(self, a: Self::NodeId) -> Self::Edges {
+        iter::MappedIter {
+            graph_id: a.graph_id,
+            iter: self.0[a.graph_id].edges(a.inner),
+        }
+    }
+}
+impl<'a, G: Data> IntoEdgesDirected for &'a GraphUnion<G>
+where
+    &'a G: IntoEdgesDirected<NodeId = G::NodeId, EdgeId = G::EdgeId, EdgeWeight = G::EdgeWeight>,
+{
+    type EdgesDirected = iter::MappedIter<<&'a G as IntoEdgesDirected>::EdgesDirected>;
+
+    fn edges_directed(self, a: Self::NodeId, dir: Direction) -> Self::EdgesDirected {
+        iter::MappedIter {
+            graph_id: a.graph_id,
+            iter: self.0[a.graph_id].edges_directed(a.inner, dir),
+        }
+    }
+}
+impl<G: Visitable> Visitable for GraphUnion<G> {
+    type Map = UnionVisitMap<G::Map>;
+
+    fn visit_map(&self) -> Self::Map {
+        UnionVisitMap(self.0.iter().map(G::visit_map).collect())
+    }
+    fn reset_map(&self, map: &mut Self::Map) {
+        self.0
+            .iter()
+            .zip(&mut map.0)
+            .for_each(|(g, m)| g.reset_map(m));
+    }
+}
+
+pub struct UnionVisitMap<M>(Vec<M>);
+impl<N, M: VisitMap<N>> VisitMap<Tagged<N>> for UnionVisitMap<M> {
+    fn visit(&mut self, a: Tagged<N>) -> bool {
+        self.0[a.graph_id].visit(a.inner)
+    }
+    fn is_visited(&self, a: &Tagged<N>) -> bool {
+        self.0[a.graph_id].is_visited(&a.inner)
+    }
+}
+
+#[doc(hidden)]
+pub mod iter {
+    use super::*;
+    pub struct TaggedIter<'a, G, I> {
+        pub(super) graphs: &'a [G],
+        pub(super) index: usize,
+        pub(super) iter: Option<I>,
+        pub(super) map: fn(&'a G) -> I,
+    }
+    impl<'a, G, I: Iterator> Iterator for TaggedIter<'a, G, I> {
+        type Item = Tagged<I::Item>;
+
+        fn next(&mut self) -> Option<Self::Item> {
+            loop {
+                if let Some(elem) = self.iter.as_mut()?.next() {
+                    return Some(Tagged {
+                        graph_id: self.index,
+                        inner: elem,
+                    });
+                }
+                self.index += 1;
+                self.iter = self.graphs.get(self.index).map(self.map);
+            }
+        }
+    }
+    pub struct MappedIter<I> {
+        pub(super) graph_id: usize,
+        pub(super) iter: I,
+    }
+    impl<I: Iterator> Iterator for MappedIter<I> {
+        type Item = Tagged<I::Item>;
+
+        fn next(&mut self) -> Option<Self::Item> {
+            self.iter.next().map(|inner| Tagged {
+                graph_id: self.graph_id,
+                inner,
+            })
+        }
+    }
+}
