a36e06fc4337c396696742024c65375c1af47098
Matthew Cornell <mcornell400708@gmail.com>
2024-10-05 10:45:54 -0400

    Add a logging UI

--- src/bin/gui.rs
+++ src/bin/gui.rs
@@ -1,5 +1,7 @@
 use eframe::egui;
 use lute::prelude::*;
+use tracing::level_filters::LevelFilter;
+use tracing_subscriber::{filter, prelude::*, Registry};
 
 #[cfg(not(target_family = "wasm"))]
 fn main() -> eframe::Result {
@@ -7,117 +9,153 @@ fn main() -> eframe::Result {
     let mut current = 0;
     let mut smiles_input = String::new();
     let mut smiles_popup_focus = true;
+    let mut show_logs = false;
+    let collector = egui_tracing::EventCollector::new();
+    Registry::default()
+        .with(
+            filter::Targets::new()
+                .with_default(LevelFilter::INFO)
+                .with_target("lute", LevelFilter::TRACE)
+                .with_target("gui", LevelFilter::TRACE),
+        )
+        .with(collector.clone())
+        .init();
     eframe::run_simple_native(
         "Lute GUI",
         eframe::NativeOptions::default(),
         move |ctx, _frame| {
-            if ctx.input_mut(|i| {
-                i.consume_shortcut(&egui::KeyboardShortcut::new(
+            let close = ctx.input_mut(|i| {
+                let close = i.consume_shortcut(&egui::KeyboardShortcut::new(
                     egui::Modifiers::CTRL,
                     egui::Key::W,
-                ))
-            }) {
+                ));
+                if i.consume_shortcut(&egui::KeyboardShortcut::new(
+                    egui::Modifiers::CTRL,
+                    egui::Key::D,
+                )) {
+                    show_logs = !show_logs;
+                }
+                close
+            });
+            if close {
                 ctx.send_viewport_cmd(egui::ViewportCommand::Close);
             }
-            egui::SidePanel::left("sidebar").show(ctx, |ui| {
-                ui.horizontal_top(|ui| {
-                    ui.label("Fragments");
-                    ui.with_layout(egui::Layout::right_to_left(egui::Align::Min), |ui| {
-                        if ui.button("C").clicked()
-                            || ctx.input_mut(|i| {
-                                i.consume_shortcut(&egui::KeyboardShortcut::new(
-                                    egui::Modifiers::CTRL,
-                                    egui::Key::R,
-                                ))
-                            })
-                        {
-                            arena = Arena::new();
-                        }
-                        {
-                            let popup_id = ui.make_persistent_id("load_smiles");
-                            let response = ui.button("+");
-                            if response.clicked()
-                                || (ctx.memory(|mem| mem.focused().is_none())
-                                    && ctx.input_mut(|i| {
-                                        i.consume_key(Default::default(), egui::Key::L)
-                                    }))
+            egui::SidePanel::left("sidebar")
+                .resizable(true)
+                .show(ctx, |ui| {
+                    ui.horizontal_top(|ui| {
+                        ui.label("Fragments");
+                        ui.with_layout(egui::Layout::right_to_left(egui::Align::Min), |ui| {
+                            if ui.button("C").clicked()
+                                || ctx.input_mut(|i| {
+                                    i.consume_shortcut(&egui::KeyboardShortcut::new(
+                                        egui::Modifiers::CTRL,
+                                        egui::Key::R,
+                                    ))
+                                })
                             {
-                                ui.memory_mut(|mem| mem.open_popup(popup_id));
-                                smiles_popup_focus = true;
+                                arena = Arena::new();
                             }
-                            egui::popup_above_or_below_widget(
-                                ui,
-                                popup_id,
-                                &response,
-                                egui::AboveOrBelow::Below,
-                                egui::PopupCloseBehavior::IgnoreClicks,
-                                |ui| {
-                                    ui.set_min_width(100.0);
-                                    let editor = ui.add(
-                                        egui::TextEdit::singleline(&mut smiles_input)
-                                            .font(egui::TextStyle::Monospace)
-                                            .return_key(None),
-                                    );
-                                    if smiles_popup_focus {
-                                        editor.request_focus();
-                                        smiles_popup_focus = false;
-                                    }
-                                    ui.with_layout(
-                                        egui::Layout::top_down_justified(egui::Align::Center),
-                                        |ui| {
-                                            let load = ui.button("Load");
-                                            let cancel = ui.button("Cancel");
-                                            if load.clicked()
-                                                || (editor.has_focus()
-                                                    && ctx.input_mut(|i| {
-                                                        i.consume_key(
-                                                            Default::default(),
-                                                            egui::Key::Enter,
-                                                        )
-                                                    }))
-                                            {
-                                                let graph = SmilesParser::new(&smiles_input)
-                                                    .parse()
-                                                    .expect("TODO");
-                                                let idx = arena.insert_mol(&graph);
-                                                current = idx.0;
-                                                smiles_input.clear();
-                                                ui.memory_mut(|mem| mem.close_popup());
-                                            } else if cancel.clicked()
-                                                || (editor.has_focus()
-                                                    && ctx.input_mut(|i| {
-                                                        i.consume_key(
-                                                            Default::default(),
-                                                            egui::Key::Escape,
-                                                        )
-                                                    }))
-                                            {
-                                                smiles_input.clear();
-                                                ui.memory_mut(|mem| mem.close_popup());
-                                            }
-                                        },
-                                    );
-                                },
-                            );
-                        }
+                            {
+                                let popup_id = ui.make_persistent_id("load_smiles");
+                                let response = ui.button("+");
+                                if response.clicked()
+                                    || (ctx.memory(|mem| mem.focused().is_none())
+                                        && ctx.input_mut(|i| {
+                                            i.consume_key(Default::default(), egui::Key::L)
+                                        }))
+                                {
+                                    ui.memory_mut(|mem| mem.open_popup(popup_id));
+                                    smiles_popup_focus = true;
+                                }
+                                egui::popup_above_or_below_widget(
+                                    ui,
+                                    popup_id,
+                                    &response,
+                                    egui::AboveOrBelow::Below,
+                                    egui::PopupCloseBehavior::IgnoreClicks,
+                                    |ui| {
+                                        ui.set_min_width(100.0);
+                                        let editor = ui.add(
+                                            egui::TextEdit::singleline(&mut smiles_input)
+                                                .font(egui::TextStyle::Monospace)
+                                                .return_key(None),
+                                        );
+                                        if smiles_popup_focus {
+                                            editor.request_focus();
+                                            smiles_popup_focus = false;
+                                        }
+                                        ui.with_layout(
+                                            egui::Layout::top_down_justified(egui::Align::Center),
+                                            |ui| {
+                                                let load = ui.button("Load");
+                                                let cancel = ui.button("Cancel");
+                                                if load.clicked()
+                                                    || (editor.has_focus()
+                                                        && ctx.input_mut(|i| {
+                                                            i.consume_key(
+                                                                Default::default(),
+                                                                egui::Key::Enter,
+                                                            )
+                                                        }))
+                                                {
+                                                    match SmilesParser::new(&smiles_input).parse() {
+                                                        Ok(graph) => {
+                                                            let idx = arena.insert_mol(&graph);
+                                                            current = idx.0;
+                                                        }
+                                                        Err(err) => tracing::error!(
+                                                            err = &err as &(dyn std::error::Error
+                                                                  + Send
+                                                                  + Sync
+                                                                  + 'static),
+                                                            "error parsing SMILES"
+                                                        ),
+                                                    }
+                                                    smiles_input.clear();
+                                                    ui.memory_mut(|mem| mem.close_popup());
+                                                } else if cancel.clicked()
+                                                    || (editor.has_focus()
+                                                        && ctx.input_mut(|i| {
+                                                            i.consume_key(
+                                                                Default::default(),
+                                                                egui::Key::Escape,
+                                                            )
+                                                        }))
+                                                {
+                                                    smiles_input.clear();
+                                                    ui.memory_mut(|mem| mem.close_popup());
+                                                }
+                                            },
+                                        );
+                                    },
+                                );
+                            }
+                        });
+                    });
+                    ui.separator();
+                    egui::ScrollArea::vertical().show(ui, |ui| {
+                        ui.with_layout(egui::Layout::top_down_justified(egui::Align::Min), |ui| {
+                            for n in 0..arena.expose_frags().len() {
+                                let n = n as u32;
+                                let button = ui.button(format!(
+                                    "{n}: {}",
+                                    arena.molecule(n.into()).smiles(Default::default())
+                                ));
+                                if button.clicked() {
+                                    current = n;
+                                }
+                            }
+                        });
                     });
                 });
-                ui.separator();
-                egui::ScrollArea::vertical().show(ui, |ui| {
-                    ui.with_layout(egui::Layout::top_down_justified(egui::Align::Min), |ui| {
-                        for n in 0..arena.expose_frags().len() {
-                            let n = n as u32;
-                            let button = ui.button(format!(
-                                "{n}: {}",
-                                arena.molecule(n.into()).smiles(Default::default())
-                            ));
-                            if button.clicked() {
-                                current = n;
-                            }
-                        }
+            if show_logs {
+                egui::TopBottomPanel::bottom("logs")
+                    .resizable(true)
+                    .show(ctx, |ui| {
+                        ui.add(egui_tracing::Logs::new(collector.clone()));
                     });
-                });
-            });
+            }
             egui::CentralPanel::default().show(ctx, |ui| {
                 if !arena.expose_frags().is_empty() {
                     ui.centered_and_justified(|ui| {
